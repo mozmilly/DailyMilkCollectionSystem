c2fb50d2233766e9eb6b816bad796fe8d38ded03
moses <mozdemilly@gmail.com>
2019-01-21 16:32:19 +0300

    .

--- src/java/servlets/Apply.java
+++ src/java/servlets/Apply.java
@@ -6,24 +6,24 @@
 package servlets;
 
 import java.io.IOException;
-import java.util.List;
+import java.io.InputStream;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import models.User;
-import org.hibernate.Query;
+import models.Applicant;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.mindrot.jbcrypt.BCrypt;
-import static servlets.Login.factory;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author moses
  */
 @WebServlet("/apply")
+@MultipartConfig(maxFileSize = 16177215) 
 public class Apply extends HttpServlet {
  
     public static SessionFactory factory;
@@ -38,17 +38,29 @@ public class Apply extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Session session = factory.openSession();
         try{
+            Session session = factory.openSession();
+            Transaction transaction = session.beginTransaction();
             String firstname = req.getParameter("firstname");
             String lastname = req.getParameter("lastname");
             String email = req.getParameter("email");
             int nationalId = Integer.parseInt(req.getParameter("id"));
             int phone = Integer.parseInt(req.getParameter("phonenumber"));
+            
+            InputStream is = req.getPart("document").getInputStream();
+            byte[] document = new byte[1024];
+            is.read(document);
+            System.out.println("error in saving");
+            Applicant applicant = new Applicant(nationalId, firstname, lastname, phone, email, document, true);
+            session.saveOrUpdate(applicant);
+            transaction.commit();
+            session.close();
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+            
         }catch(Exception e){
             error = e.getLocalizedMessage();
             req.setAttribute("error", error);
-            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+            
         }
     }
     
